b241eaf0d833d6609318406729ef35a1e4abd2f5
Arsbul <ars.bulygin@yandex.ru>
2023-03-29 12:39:56 +0300

    Полная логика главной страницы

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import random
 import time
 from datetime import datetime
+from pprint import pprint
 
 from flask import render_template, redirect
 import requests
@@ -22,12 +23,20 @@ def load_user(user_id):
 @app.route("/index")
 @login_required
 def index():
+    cells_names = {
+        1: "Выкладка фишек",
+        2: "Сортировка",
+        3: "Маркировка",
+        4: "Выкладка оснований",
+        5: "Раскладка",
+        6: "Упаковка"
+    }
     cells_status = requests.get("http://roboprom.kvantorium33.ru/api/current").json()
 
     work_procent = []
-    for d in cells_status["data"]:
-        for param in d["params"]:
-            if param["param"] == "status" and param["value"]:
+    for table_cell in cells_status["data"]:
+        for param in table_cell["params"]:
+            if param["param"] == "status" and param["value"] == 1:
                 w = int(time.time()) - param["timestamp"]
                 work_procent.append((w / 3600) * 100)
     if work_procent:
@@ -137,7 +146,7 @@ def index():
     for hour in range(24):
         params = {
             "from": int(time.time()) - datetime.now().hour * 60 * 60,
-            "to": int(time.time()) - datetime.now().hour * 60 * 60 + hour * 60 * 60,
+
             "param": "count",
             "cell": 6
         }
@@ -148,8 +157,79 @@ def index():
             if i["param"] == "count":
                 h += i["value"]
         graph.append(h)
+    # таблица
+    table_data = []
+
+    query = requests.get("http://roboprom.kvantorium33.ru/api/current").json()
+
+    k = {
+        "wait": {
+            0: "Нет ожидания",
+            1: "Ожидание заготовок на входе",
+            2: "линия переполнена или ожидание готовности следующей ячейки"
+        },
+        "status": {
+            0: "выключена",
+            1: "работает",
+            2: "ожидание",
+            3: "ошибка"
+        }
+
+    }
+    for cells in query["data"]:
+        table_cell = {"index": cells["cell"],
+                      "status": "",
+                      "wait": "",
+                      "speed_hour": 0,
+                      "speed_day": 0,
+                      "work_hour": 0,
+                      "work_day": 0}
+        cell_params = cells["params"]
+        for param in cell_params:
+            if param["param"] == "status":
+                table_cell["status"] = k["status"].get(param["value"])
+            if param["param"] == "wait":
+                table_cell["wait"] = k["wait"].get(param["value"])
+        ## количество обработанных заготовок за последний час ##
+
+        params = {
+            "from": int(time.time()) - 60 * 60,
+            "cell": cells["cell"],
+            "param": "count"
+        }
+        query = requests.get("http://roboprom.kvantorium33.ru/api/history", params=params).json()
+
+        for param in query["data"]:
+            table_cell["speed_hour"] += param["value"]
+        ## количество обработанных заготовок за текущий день ##
+
+        params = {
+            "from": int(time.time()) - datetime.now().hour * 60 * 60,
+            "cell": cells["cell"],
+            "param": "count"
+        }
+        query = requests.get("http://roboprom.kvantorium33.ru/api/history", params=params).json()
+
+        for param in query["data"]:
+            table_cell["speed_day"] += param["value"]
+
+        ## загрузка оборудования за последний час ##
+        params = {
+            "cell": cells["cell"],
+        }
+        query = requests.get("http://roboprom.kvantorium33.ru/api/current", params=params).json()
+
+        for c in query["data"]:
+            for param in c["params"]:
+                if param["param"] == "status" and param["value"] == 1:
+                    w = int(time.time()) - param["timestamp"]
+                    table_cell["work_hour"] = (w / (60 * 60)) * 100
+                    table_cell["work_day"] = (w / (60 * 60 * 24)) * 100
+        table_data.append(table_cell)
 
     return render_template("index.html",
+                           cells_names=cells_names,
+                           table_data=table_data,
                            graph=graph,
                            work_procent=work_procent,
                            production_data=production_data,
